01ee1ff82a7c8b21341a1d16cc05c1796d641f21
Calebe <calebebraga08@gmail.com>
2026-03-04 20:22:04 -0300

    Configure application settings and services: add connection strings for SQL Server and Redis, integrate Swagger for API documentation, and set up dependency injection for account management services.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
+,using BancoKRT.Api.Application.Accounts;
+using BancoKRT.Api.Domain.Accounts;
+using BancoKRT.Api.Infrastructure.Messaging;
+using BancoKRT.Api.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BancoKrtContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddSingleton<IAccountEventPublisher, KafkaAccountEventPublisher>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
